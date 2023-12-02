7e088ddd37cf78779d7d990ba515502e0baf4926
re3-marco <130209302+re3-marco@users.noreply.github.com>
2023-12-02 14:23:49 +0800

    init react native

---
+++ App.tsx
@@ -0,0 +1,20 @@
+import { Text } from "react-native";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          <code>TripleHanHan</code>
+        </p>
+        </header>
+        <body>
+          <p>
+            <Text>  Hi</Text>
+          </p>
+        </body>
+    </div>
+
+    );
+}
+export default App;
